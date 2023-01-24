dbdb2bf67fce8e27e36daa1e247e8655ca1b3d9c
Александр <hyperpositron@gmail.com>
2023-01-24 17:23:02 +0200

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import javax.imageio.metadata.IIOMetadataFormatImpl;
+import java.util.Arrays;
+
 public class Main {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100000) + 100000;
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("___________________________________________Задача 1______________________________________");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        int min = 999999;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
     }
 }
